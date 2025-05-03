46bdede1510c79abaa1ac9966e9882d7b646294a
Serhiy Zabolotnyy <zabolotnyy.s@gmail.com>
2025-05-03 15:47:24 +0300

    Update l10n_xcstrings.rb

--- Formula/l10n_xcstrings.rb
+++ Formula/l10n_xcstrings.rb
@@ -4,16 +4,23 @@ class L10nXcstrings < Formula
   desc "Localization tool for xcstrings"
   homepage "https://github.com/Disconnecter/L10nXcstrings"
   url "https://github.com/Disconnecter/L10nXcstrings/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "d11375f9a741bd72cf8ef249b7cc6fa7a52643c59da87ccee84140f1e4d906e1"
+  sha256 "505644a5838a81450fce5de72f4c844e8034be5ab4ea84af658c992c5291358a"
   license "MIT"
 
   depends_on "python@3.11"
 
   def install
-    virtualenv_install_with_resources
+    # Unpack into buildpath directly (flatten)
+    system "tar", "-xzf", "L10nXcstrings-0.0.1.tar.gz", "-C", buildpath, "--strip-components=1"
+
+    system "#{Formula["python@3.11"].opt_bin}/python3", "-m", "venv", libexec
+    system libexec/"bin/python", "-m", "pip", "install", ".", "--no-deps"
+    bin.install_symlink libexec/"bin/l10n-xcstrings"
   end
 
   test do
     system "#{bin}/l10n-xcstrings", "--help"
   end
 end
+
+
