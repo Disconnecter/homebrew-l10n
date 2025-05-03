fa7338afb4fca389c791ce12936400b42d08142b
Serhiy Zabolotnyy <zabolotnyy.s@gmail.com>
2025-05-03 16:17:06 +0300

    Update l10n_xcstrings.rb

--- Formula/l10n_xcstrings.rb
+++ Formula/l10n_xcstrings.rb
@@ -4,7 +4,7 @@ class L10nXcstrings < Formula
   desc "Generate Swift localization code from .xcstrings"
   homepage "https://github.com/Disconnecter/L10nXcstrings"
   url "https://github.com/Disconnecter/L10nXcstrings/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "505644a5838a81450fce5de72f4c844e8034be5ab4ea84af658c992c5291358a"
+  sha256 "d94b67ce40b61d6ef99b00d7284ab9da95f6663b9a95e364c31b2b4f0a528700"
   license "MIT"
 
   depends_on "python@3.11"
