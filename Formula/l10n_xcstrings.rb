fea3a834229f8040bf9952554c7986f077301e12
Serhiy Zabolotnyy <zabolotnyy.s@gmail.com>
2025-05-03 15:31:53 +0300

    Update l10n_xcstrings.rb

--- Formula/l10n_xcstrings.rb
+++ Formula/l10n_xcstrings.rb
@@ -4,7 +4,7 @@ class L10nXcstrings < Formula
   desc "Localization tool for xcstrings"
   homepage "https://github.com/Disconnecter/L10nXcstrings"
   url "https://github.com/Disconnecter/L10nXcstrings/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "8786d36cc6cb145c0cb2a392e9c9915600a53e649416e24acb62de43be76dd91"
+  sha256 "d11375f9a741bd72cf8ef249b7cc6fa7a52643c59da87ccee84140f1e4d906e1"
   license "MIT"
 
   depends_on "python@3.11"
