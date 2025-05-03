deb4f7ce75d905b3fb9ef9b60dc149ba09feaca6
Serhiy Zabolotnyy <zabolotnyy.s@gmail.com>
2025-05-03 16:04:47 +0300

    Update l10n_xcstrings.rb

--- Formula/l10n_xcstrings.rb
+++ Formula/l10n_xcstrings.rb
@@ -1,30 +1,19 @@
 class L10nXcstrings < Formula
   include Language::Python::Virtualenv
 
-  desc "Generate Swift code from xcstrings files"
+  desc "Generate Swift localization code from .xcstrings"
   homepage "https://github.com/Disconnecter/L10nXcstrings"
   url "https://github.com/Disconnecter/L10nXcstrings/archive/refs/tags/0.0.1.tar.gz"
   sha256 "505644a5838a81450fce5de72f4c844e8034be5ab4ea84af658c992c5291358a"
   license "MIT"
-  head "https://github.com/Disconnecter/L10nXcstrings.git", branch: "main"
 
   depends_on "python@3.11"
 
   def install
-    tarball = cached_download
-    build_dir = buildpath/"src"
-
-    # Extract archive and strip the top-level folder
-    system "tar", "-xzf", tarball, "-C", build_dir, "--strip-components=1"
-
-    cd build_dir do
-      venv = virtualenv_create(libexec, "python3.11")
-      venv.pip_install_and_link buildpath/"src"
-    end
+    virtualenv_install_with_resources
   end
 
   test do
-    output = shell_output("#{bin}/l10n-xcstrings --help")
-    assert_match "usage", output
+    system "#{bin}/l10n-xcstrings", "--help"
   end
 end
